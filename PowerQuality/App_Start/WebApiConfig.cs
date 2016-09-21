1471e51c9f4af83cbc5c763e0ac8105a610ac919
张砚秋 <autyan@outlook.com>
2016-09-21 15:03:56 +0800

    新建电力质量分析项目。

--- PowerQuality/App_Start/WebApiConfig.cs
+++ PowerQuality/App_Start/WebApiConfig.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Net.Http.Headers;
 using System.Web.Http;
 
 namespace PowerQuality
@@ -19,6 +17,8 @@ namespace PowerQuality
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+
+            config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
         }
     }
 }
