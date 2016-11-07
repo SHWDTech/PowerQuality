da89ce7fa3676b302d7888077483b40f650ef402
Alex <autyan@outlook.com>
2016-11-08 01:00:56 +0800

    添加谐波界面。

--- PowerQuality/Scripts/function/powerAnalysis.js
+++ PowerQuality/Scripts/function/powerAnalysis.js
@@ -1,9 +1,13 @@
 ﻿var loadPercentage = 0;
 
+var voltageCurrentCharts = [];
+var harmonicCharts = [];
+
 var chartsConfig = {
     'scale': 0,
     'maxPoint': 0,
-    'dataSource': null,
+    'voltageCUrrentDataSource': null,
+    'harmonicDataSource': null,
     start: 0,
     end: 14400
 }
@@ -55,7 +59,9 @@ var powerDatas = {
 };
 
 var powerAnalysis = {
-    'selectChannel': [],
+    'voltageCurrentChannel': [],
+    'harmonicChannel': [],
+    'harmonicClass': [],
     'fetch': function (url, startIndex, totalRequest, target) {
         if (totalRequest <= 0) {
             loadPercentage += 25;
@@ -74,46 +80,48 @@ var powerAnalysis = {
         $('#percentage').html(loadPercentage);
         if (loadPercentage === 100) {
             powerAnalysis.setupConfig();
-            powerAnalysis.initCharts();
+            powerAnalysis.initVoltageCurrentCharts();
+            powerAnalysis.initHarmonicCharts();
             $('#pageLoading').hide('slow');
+            $('#voltageCurrent').show();
             $('#mainContent').show('slow');
         }
     },
-    'initCharts': function () {
-        window.powerCharts.forEach(function(chart) {
+    'initVoltageCurrentCharts': function () {
+        voltageCurrentCharts.forEach(function (chart) {
             echarts.dispose(chart);
         });
-        window.powerCharts = [];
-        $('#chartContainer').empty();
+        voltageCurrentCharts = [];
+        $('#voltageCurrentCharts').empty();
         var maxHeight = 800;
-        var chartHeights = maxHeight / powerAnalysis.selectChannel.length;
-        powerAnalysis.selectChannel.forEach(function (channel, index) {
+        var chartHeights = maxHeight / powerAnalysis.voltageCurrentChannel.length;
+        powerAnalysis.voltageCurrentChannel.forEach(function (channel, index) {
             var chart = document.createElement('div');
             $(chart).attr('id', channel + '_chart');
-            $('#chartContainer').append(chart);
+            $('#voltageCurrentCharts').append(chart);
             $(chart).height(chartHeights);
             $(chart).width($('#chartHolder').width() - 50);
             var current = echarts.init(chart);
             if (index !== 0) {
                 $(chart).css('margin-top', '-20px');
             }
-            window.powerCharts.push(current);
-            current.setOption(powerAnalysis.getChartOption(channel, index));
+            voltageCurrentCharts.push(current);
+            current.setOption(powerAnalysis.getVoltageCurrentChartOption(channel, index));
         });
-        echarts.connect(window.powerCharts);
+        echarts.connect(voltageCurrentCharts);
 
-        window.powerCharts.forEach(function(chart) {
+        voltageCurrentCharts.forEach(function (chart) {
             chart.on('datazoom', function (params) {
                 var start = params.batch[0].startValue;
                 var end = params.batch[0].endValue;
                 if (chartsConfig.scale === 3 && (end - start) * 3 < chartsConfig.maxPoint) {
-                    chartsConfig.dataSource = powerDatas.voltageCurrentSecond;
+                    chartsConfig.voltageCUrrentDataSource = powerDatas.voltageCurrentSecond;
                     chartsConfig.start = start * 3;
                     chartsConfig.end = end * 3;
                     chartsConfig.scale = 1;
-                    powerAnalysis.initCharts();
+                    powerAnalysis.initVoltageCurrentCharts();
                 } else {
-                    window.powerCharts.forEach(function (crt) {
+                    voltageCurrentCharts.forEach(function (crt) {
                         var option = crt.getOption();
                         var arr = option.series[0].data.slice(start, end)
                             .concat(option.series[1].data.slice(start, end))
@@ -130,15 +138,59 @@ var powerAnalysis = {
             });
         });
     },
-    'getChartOption': function (channel, index) {
-        var max = chartsConfig.dataSource[channel + '_Max'].slice(chartsConfig.start, chartsConfig.end);
-        var avg = chartsConfig.dataSource[channel + '_Avg'].slice(chartsConfig.start, chartsConfig.end);
-        var min = chartsConfig.dataSource[channel + '_Min'].slice(chartsConfig.start, chartsConfig.end);
+    'initHarmonicCharts': function () {
+        harmonicCharts.forEach(function (chart) {
+            echarts.dispose(chart);
+        });
+        harmonicCharts = [];
+        $('#harmonicCharts').empty();
+        var maxHeight = 800;
+        var chartHeights = maxHeight / powerAnalysis.harmonicChannel.length;
+        powerAnalysis.harmonicChannel.forEach(function (channel, index) {
+            var chart = document.createElement('div');
+            $(chart).attr('id', channel + '_chart');
+            $('#harmonicCharts').append(chart);
+            $(chart).height(chartHeights);
+            $(chart).width($('#chartHolder').width() - 50);
+            var current = echarts.init(chart);
+            if (index !== 0) {
+                $(chart).css('margin-top', '-20px');
+            }
+            harmonicCharts.push(current);
+            current.setOption(powerAnalysis.getHarmonicChartOption(channel, index));
+        });
+        echarts.connect(harmonicCharts);
+
+        harmonicCharts.forEach(function (chart) {
+            chart.on('datazoom', function (params) {
+                var start = params.batch[0].startValue;
+                var end = params.batch[0].endValue;
+                harmonicCharts.forEach(function (crt) {
+                    var option = crt.getOption();
+                    var arr = option.series[0].data.slice(start, end)
+                        .concat(option.series[1].data.slice(start, end))
+                        .concat(option.series[2].data.slice(start, end));
+                    var max = Math.max.apply(null, arr);
+                    var min = Math.min.apply(null, arr);
+                    var interval = (max - min) / 4;
+                    option.yAxis[0].max = Math.round((max + interval) * 100) / 100;
+                    option.yAxis[0].min = Math.round((min - interval) * 100) / 100;
+                    option.yAxis[0].interval = Math.round(interval * 100) / 100;
+                    crt.setOption(option);
+                });
+
+            });
+        });
+    },
+    'getVoltageCurrentChartOption': function (channel, index) {
+        var max = chartsConfig.voltageCUrrentDataSource[channel + '_Max'].slice(chartsConfig.start, chartsConfig.end);
+        var avg = chartsConfig.voltageCUrrentDataSource[channel + '_Avg'].slice(chartsConfig.start, chartsConfig.end);
+        var min = chartsConfig.voltageCUrrentDataSource[channel + '_Min'].slice(chartsConfig.start, chartsConfig.end);
         var maxVal = Math.round(Math.max.apply(null, max.concat(avg).concat(min)) * 100) / 100;
         var minVal = Math.round(Math.min.apply(null, max.concat(avg).concat(min)) * 100) / 100;
         var interval = Math.round((maxVal - minVal) / 4 * 100) / 100;
         var par = {
-            'category': chartsConfig.dataSource['RecordTime'].slice(chartsConfig.start, chartsConfig.end).map(function (date) {
+            'category': chartsConfig.voltageCUrrentDataSource['RecordTime'].slice(chartsConfig.start, chartsConfig.end).map(function (date) {
                 return new Date(date);
             }),
             'max': max,
@@ -152,42 +204,100 @@ var powerAnalysis = {
             'titleName': channel
         }
 
-        if (index < powerAnalysis.selectChannel.length - 1) {
+        if (index < powerAnalysis.voltageCurrentChannel.length - 1) {
             par['axisLabel'] = false;
         }
 
         return chartsOption.StepLine(par);
     },
-    'setChannels': function (selects) {
-        powerAnalysis.selectChannel = [];
+    'getHarmonicChartOption': function (channel, index) {
+        var series = powerAnalysis.prepareSeries(channel);
+        var par = {
+            'category': chartsConfig.harmonicDataSource['RecordTime'].slice(chartsConfig.start, chartsConfig.end).map(function (date) {
+                return new Date(date);
+            }),
+            'axisLabel': true,
+            'index': index,
+            'titleName': channel,
+            'series': series,
+            'legend': powerAnalysis.harmonicClass
+        }
+
+        if (index < powerAnalysis.voltageCurrentChannel.length - 1) {
+            par['axisLabel'] = false;
+        }
+
+        return chartsOption.LineChart(par);
+    },
+    'prepareSeries': function (channel) {
+        var series = [];
+        powerAnalysis.harmonicClass.forEach(function(harmonicClass) {
+            var params = {
+                'name': harmonicClass,
+                'data': chartsConfig.harmonicDataSource[channel + 'Harmonic' + harmonicClass].map(function (har) {
+                    return har * 100;
+                })
+            }
+            series.push(chartsOption.LineSeries(params));
+        });
+
+        return series;
+    },
+    'setVoltageCurrentChannels': function () {
+        var selects = $('#channelOptions input[type="checkbox"]');
+        powerAnalysis.voltageCurrentChannel = [];
         $.each(selects, function (key, value) {
             if ($(value).is(':checked')) {
-                powerAnalysis.selectChannel.push($(value).attr('id'));
+                powerAnalysis.voltageCurrentChannel.push($(value).attr('id'));
+            }
+        });
+    },
+    'setHarmonicChannels': function () {
+        var selects = $('#harmonicChannelOptions input[type="checkbox"]');
+        powerAnalysis.harmonicChannel = [];
+        $.each(selects, function (key, value) {
+            if ($(value).is(':checked')) {
+                powerAnalysis.harmonicChannel.push($(value).attr('id'));
+            }
+        });
+        powerAnalysis.harmonicClass = [];
+        var classSelects = $('#harmonicClassOptions input[type="checkbox"]');
+        $.each(classSelects, function (key, value) {
+            if ($(value).is(':checked')) {
+                powerAnalysis.harmonicClass.push($(value).attr('id'));
             }
         });
     },
     'setupConfig': function () {
-        powerAnalysis.setChannels($('#channelOptions input[type="checkbox"]'));
+        powerAnalysis.setVoltageCurrentChannels();
+        powerAnalysis.setHarmonicChannels();
         chartsConfig.maxPoint = $('#chartHolder').width() - 100;
         if (powerDatas.voltageCurrentSecond['RecordTime'].length > chartsConfig.maxPoint) {
-            chartsConfig.dataSource = powerDatas.voltageCurrentThreeSecond;
+            chartsConfig.voltageCUrrentDataSource = powerDatas.voltageCurrentThreeSecond;
             chartsConfig.scale = 3;
         } else {
-            chartsConfig.dataSource = powerDatas.voltageCurrentSecond;
+            chartsConfig.voltageCUrrentDataSource = powerDatas.voltageCurrentSecond;
             chartsConfig.scale = 1;
         }
+        chartsConfig.harmonicDataSource = powerDatas.harmonics;
     }
 };
 
 $(function () {
-    $('#channelOptions input[type="checkbox"]').on('change',function(event) {
+    $('#channelOptions input[type="checkbox"]').on('change', function (event) {
         if ($(event.target).attr('data-vol-type') === 'star') {
             $('[data-vol-type="triangle"]').prop('checked', false);
         } else if ($(event.target).attr('data-vol-type') === 'triangle') {
             $('[data-vol-type="star"]').prop('checked', false);
         }
-        powerAnalysis.setChannels($('#channelOptions input[type="checkbox"]'));
-        powerAnalysis.initCharts();
+        powerAnalysis.setVoltageCurrentChannels();
+        powerAnalysis.initVoltageCurrentCharts();
+    });
+    $('.navtab').on('click', function (nav) {
+        $('.navtab').removeClass('active');
+        $('.presentDiv').hide();
+        $(nav.target).parent('li').addClass('active');
+        $('#' + $(nav.target).attr('data-linkdiv')).show();
     });
     powerAnalysis.fetch('/PowerAnalysis/RecordData', 0, 14400, powerDatas.activeValues, powerAnalysis.loadComplete());
     powerAnalysis.fetch('/PowerAnalysis/RecordHarmonic', 0, 14400, powerDatas.harmonics, powerAnalysis.loadComplete());
--- PowerQuality/Scripts/function/powercharts.js
+++ PowerQuality/Scripts/function/powercharts.js
@@ -1,4 +1,4 @@
-﻿var powerCharts = [];
+﻿var voltageCurrentCharts = [];
 var chartsOption = {};
 
 chartsOption.StepLine = function (params) {
@@ -123,6 +123,103 @@ chartsOption.StepLine = function (params) {
     }
     return opt;
 };
+
+chartsOption.LineChart = function (params) {
+    var opt = {
+        title: {
+            text: params['titleName'],
+            show: true,
+            textStyle: {
+                color: '#337ab7',
+                fontSize: 14
+            },
+            textAlign: 'center',
+            left: 'center'
+        },
+        tooltip: {
+            trigger: 'axis',
+            formatter: function (formatParams) {
+                return params['category'][formatParams[0]['dataIndex']].format("yyyy-MM-dd hh:mm:ss S")
+                + '<br/>' + '<span style="display:inline-block;margin-right:5px;border-radius:10px;width:9px;height:9px;background-color:' + formatParams[0].color + '"></span>' + formatParams[0]['seriesName'] + '：' + formatParams[0].data + '%'
+                + '<br/>' + '<span style="display:inline-block;margin-right:5px;border-radius:10px;width:9px;height:9px;background-color:' + formatParams[1].color + '"></span>' + formatParams[1]['seriesName'] + '：' + formatParams[1].data + '%';
+            }
+        },
+        legend: {
+            show: false,
+            data: params['legend']
+        },
+        grid: {
+            top: '0%',
+            left: '0%',
+            right: '0%',
+            bottom: '0%',
+            containLabel: true
+        },
+        toolbox: {
+        },
+        xAxis: {
+            type: 'category',
+            data: params['category'],
+            axisLabel: {
+                show: params['axisLabel'],
+                formatter: function (value) {
+                    return value.format("hh:mm:ss S");
+                }
+            }
+        },
+        yAxis: {
+            type: 'value',
+            splitNumber: 6,
+            min: 0,
+            minInterval: 0.001,
+            axisLabel: {
+                show: true
+            }
+        },
+        dataZoom: {
+            type: 'inside',
+            yAxisIndex: null,
+            filterMode: 'filter',
+            zoomLock: true
+        },
+        series: params['series']
+    };
+
+    opt['toolbox'] = {
+        show: true,
+        feature: {
+            dataZoom: {
+                show: true,
+                yAxisIndex: false
+            },
+            restore: {
+                show: true
+            }
+        }
+    };
+    opt.grid.top = '25px';
+    if (params['index'] !== 0) {
+        opt.toolbox.top = -10000;
+    }
+    return opt;
+};
+
+chartsOption.LineSeries = function(params) {
+    var serier = {
+        name: params['name'],
+        type: 'line',
+        xAxisIndex: 0,
+        yAxisIndex: 0,
+        lineStyle: {
+            normal: {
+                width: 1
+            }
+        },
+        data: params['data']
+    }
+
+    return serier;
+}
 
 chartsOption.pushData = function () {
 
